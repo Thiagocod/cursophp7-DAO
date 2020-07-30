610e06848cc6a4bb406e7c239f5a3fffa4080471
Thiago Vieira <thiagoviera0007@yahoo.com.br>
2020-07-30 15:08:40 -0300

    Finalizamos a aula 66

--- class/usuario.php
+++ class/usuario.php
@@ -42,6 +42,24 @@ class Usuario{
       $this->setDtcadastro(new DateTime($row['dtcadastro']));
     }
   }
+
+  public static function getList(){
+    $sql = new Sql();
+
+    return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+  }
+  public static function search($login){
+
+    $sql = new Sql();
+    return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+      ':SEARCH'=>"%".$login."%"
+    ));
+
+
+
+
+  }
   public function __toString(){
 
     return json_encode(array(
@@ -50,10 +68,24 @@ class Usuario{
       "dessenha"=>$this->getDessenha(),
       "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
     ));
+    }
+
+  public function login($login,$password){
+    $sql = new Sql();
+    $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = PASSWORD",array(":LOGIN"=>$login,":PASSWORD"=>$password));
+    if (count($results) > 0){
+      $row = $results[0];
+      $this->setIdusuario($row['idusuario']);
+      $this->setDeslogin($row['deslogin']);
+      $this->setDessenha($row['dessenha']);
+      $this->setDtcadastro(new DateTime($row['dtcadastro']));
+  }else{
+    throw new Exception("Login e/ou senha inválidos.");
   }
+  }
+
+
 }
-
-
 
 
 
--- index.php
+++ index.php
@@ -7,13 +7,29 @@ require_once("config.php");
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
-
+/*carrega um lista de usuários
 $root = new Usuario();
-
 $root->loadById(3);
+echo $root;*/
 
-echo $root;
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+// carrega uma lista de usuarios buscando pelo $login
+
+$search = Usuario::search("ro");
+
+echo json_encode($search);
+
+echo "<hr>";
+
+echo "Carrega um usuário usando o login e a senha";
+
+//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root","!@#$");
+
+echo $usuario;
 
 
 ?>
