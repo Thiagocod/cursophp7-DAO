45cc700d150b4d867f2dc597495799dd7f03cc60
Thiago Vieira <thiagoviera0007@yahoo.com.br>
2020-08-06 20:16:56 -0300

    Feito processos no banco de dados como excluir

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once("config.php");
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);*/
-/*carrega um lista de usuários
+/*carrega uma lista de usuários
 $root = new Usuario();
 $root->loadById(3);
 echo $root;*/
@@ -33,17 +33,29 @@ echo $usuario;
 
 echo "<hr>";
 
-echo "Inseri um novo usuario no banco de dados<br>";
-$aluno = new Usuario("aluno02","@alun02");
+//echo "Inseri um novo usuario no banco de dados<br>";
+//$aluno = new Usuario("aluno02","@alun02");
 
-$aluno->insert();
+//$aluno->insert();
 
-echo $aluno;
+//echo $aluno;
 
 echo "<hr>";
+echo "Alterar um Usuario";
 $update = new Usuario();
 $update->loadById(3);
 $update->update("root2","senhaqualquer");
+echo $update;
+echo "<hr>";
 
+echo "Deletar um usuario";
+
+$del = new Usuario();
+
+$del->loadById(15);
+
+$del->delete();
+
+echo $del;
 
 ?>
